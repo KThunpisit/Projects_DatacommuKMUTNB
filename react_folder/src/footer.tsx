3ecb9a7dd18de99b065f591c981b41045785127a
Agos4546(Thunpisit) <163816870+KThunpisit@users.noreply.github.com>
2025-09-11 01:10:15 +0700

    ทดสอบการใช้ React เเละ Github

---
+++ react_folder/src/footer.tsx
@@ -0,0 +1,11 @@
+function Footer(){
+    return(
+        <footer>
+            <h2>
+                <hr/>
+                <p>123456</p>
+            </h2>
+        </footer>
+    );
+}
+export default Footer;
---
+++ react_folder/src/header.tsx
@@ -0,0 +1,16 @@
+function Header(){
+
+    return(
+        <header>
+            <h1>my Web</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Service</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+export default Header
